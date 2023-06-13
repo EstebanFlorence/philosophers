a007476c05733c6ac331c4239a86aef97c8a4e1b
Esteban Florence <asap.bacco@protonmail.com>
2023-06-13 22:55:06 +0200

    timestamp print update

--- philo/src/threads.c
+++ philo/src/threads.c
@@ -6,7 +6,7 @@
 /*   By: adi-nata <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 22:26:41 by adi-nata          #+#    #+#             */
-/*   Updated: 2023/06/13 22:06:59 by adi-nata         ###   ########.fr       */
+/*   Updated: 2023/06/13 22:53:37 by adi-nata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,21 @@ void	ft_mutex_innit(t_table *table)
 void	ft_status(t_table *table, char *message, int id)
 {
 	struct timeval	time;
-	long			millisec;
+	long			milliseconds;
+	long			seconds;
 
 	gettimeofday(&time, NULL);
-	millisec = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+	milliseconds = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+
+    seconds = time.tv_sec % 60;
+
 	pthread_mutex_lock(&table->status);
-	printf("%ld %d %s\n", millisec, id, message);
+    printf("%02ld.%03ld %d %s\n", seconds, milliseconds % 1000, id, message);
 	pthread_mutex_unlock(&table->status);
+
+/* 	pthread_mutex_lock(&table->status);
+	printf("%ld %d %s\n", millisec, id, message);
+	pthread_mutex_unlock(&table->status); */
 }
 
 void	ft_forker(t_philo *philo)
@@ -55,8 +63,12 @@ void	ft_forker(t_philo *philo)
 		pthread_mutex_lock(philo->left_fork);
 		ft_status(philo->table, LFORK, philo->id);
 	}
-
 }
+
+/* void	ft_death(t_philo *philo)
+{
+
+} */
 
 void	*routine(void *arg)
 {
@@ -75,18 +87,19 @@ void	*routine(void *arg)
 		pthread_mutex_unlock(philo->left_fork);
 		pthread_mutex_unlock(philo->right_fork);
 		pthread_mutex_lock(&table->check);
-		if (ft_timedifference(table->time.tv_sec, table->time.tv_usec) >= table->die)
+		if (ft_timedifference(table->time.tv_sec, table->time.tv_usec) >= table->die * 1000)
 		{
-			ft_status(table, DIED, philo->id);
 			pthread_mutex_unlock(&table->check);
+			ft_status(table, DIED, philo->id);
+		//	ft_death(philo);
 			break;
 		}
 		//pthread_mutex_unlock(&table->check);
 		//pthread_mutex_lock(&table->check);
 		if (table->times != -1 && ++eat >= table->times)
 		{
-			printf("Philo %d is full\n", philo->id);
 			pthread_mutex_unlock(&table->check);
+			printf("Philo %d is full\n", philo->id);
 			break;
 		}
 		pthread_mutex_unlock(&table->check);
